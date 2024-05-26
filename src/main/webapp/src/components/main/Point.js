2b6b3625d5b18de16b2cd61e3276f2315e867ca0
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-05-26 09:37:05 +0900

    수정:이미지 alt추가

--- src/main/webapp/src/components/main/Point.js
+++ src/main/webapp/src/components/main/Point.js
@@ -4,7 +4,7 @@ import kori from "../../assets/코리.png";
 const Point = () => {
   return (
     <div className="flex justify-center items-center gap-4 mt-6">
-      <img className="w-24" src={kori} />
+      <img className="w-24" alt="코리" src={kori} />
       <span className="dark:text-white text-5xl font-semibold">62</span>
       <span className="text-[#AEDDE8] text-3xl font-bold">코리</span>
     </div>
