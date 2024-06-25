a30a41d32ae2c73c2d8c039b7004b843ee89d3e7
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-06-25 23:28:10 +0900

    수정:적립내역보기, UTC시간대로 설정

--- src/main/webapp/src/store/usePeriodStore.js
+++ src/main/webapp/src/store/usePeriodStore.js
@@ -2,7 +2,8 @@ import { create } from "zustand";
 
 export const usePeriodStore = create((set) => ({
   periods: [],
-  startDate: new Date(2024, 5, 24), // 시작일
+  startDate: new Date(Date.UTC(2024, 5, 24)), // UTC 기준으로 시작일 설정
+
   selectedPeriod: null,
 
   setPeriods: (periods) => set({ periods }),
