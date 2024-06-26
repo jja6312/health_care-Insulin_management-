aebbbd9b04231f548271a1865c9a055fccc35d53
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-06-26 18:03:16 +0900

    시작일 임시 수정

--- src/main/webapp/src/store/usePeriodStore.js
+++ src/main/webapp/src/store/usePeriodStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 export const usePeriodStore = create((set) => ({
   periods: [],
-  startDate: new Date(Date.UTC(2024, 5, 24)), // UTC 기준으로 시작일 설정
+  startDate: new Date(Date.UTC(2024, 5, 17)), // UTC 기준으로 시작일 설정
 
   selectedPeriod: null,
 
