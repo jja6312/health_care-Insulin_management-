dd32731abcd43960678dc0e9e785e81d5ad59056
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-06-28 13:50:32 +0900

    수정:로그아웃 버그(로그인유지하기 클릭안하고 로그아웃할때 안되는현상) 수정

--- src/main/webapp/src/utils/login/handleLogout.js
+++ src/main/webapp/src/utils/login/handleLogout.js
@@ -2,7 +2,8 @@ import { useNavigate } from "react-router-dom";
 
 const handleLogout = (navigate) => {
   sessionStorage.removeItem("accessToken");
-  localStorage.removeItem("refreshToken"); // 만약 로컬 스토리지의 리프레시 토큰도 제거해야 한다면 추가
+  sessionStorage.removeItem("refreshToken");
+  localStorage.removeItem("refreshToken");
   navigate("/login");
 };
 
