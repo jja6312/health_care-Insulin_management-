7a91ec5aa98a4603bb85060f94bf7aa475ed6a47
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-07-21 10:19:19 +0900

    재배포2

--- src/main/webapp/src/components/main/popup/Popup.js
+++ src/main/webapp/src/components/main/popup/Popup.js
@@ -91,7 +91,6 @@ const Popup = () => {
             />
           ))}
         </div>
-        {/* 추후 삭제 필요 */}
         <div className="flex">
           <a
             href="https://youtube.com/shorts/n7iYslLYuqY?feature=shared"
